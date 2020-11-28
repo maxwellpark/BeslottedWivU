cb3c496dcc874bc945c858fa9426ceae2a4c0add
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-11-28 19:02:24 +0000

    Slot class

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public bool isSpinning;
+    public char[] symbols; 
+    public Text symbolText;
+
+    private void Start()
+    {
+        symbols = new char[] { '#', '!', '@', '£', '$', '%', '&' };
+    }
+}
