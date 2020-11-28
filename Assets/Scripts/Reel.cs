1dcf4f28e33f6735c53213e633264e982b08f43c
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-11-28 19:50:54 +0000

    Renamed reel class

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+public class Reel : MonoBehaviour
 {
     public bool isSpinning;
     public float counter; 
