5e05e03340e5b8cb2117a8a2ac26413163c7759c
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-11-28 19:50:19 +0000

    Spin speed tuned

--- Assets/Scripts/SlotConstants.cs
+++ Assets/Scripts/SlotConstants.cs
@@ -1,6 +1,6 @@
 ﻿public static class SlotConstants
 {
     public static string[] symbols = new string[] { "#", "!", "@", "£", "$", "%", "&" };
-    public static float spinSpeed;
+    public static float spinSpeed = 2;
     public static int frameRate = 60; 
 }
