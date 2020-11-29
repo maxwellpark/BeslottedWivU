b0eac0141b454234b7c866dbc5138e529fc3a637
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-29 01:44:54 +0000

    Check if all reels are stopped

---
+++ Assets/Scripts/SlotMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMachine : MonoBehaviour
+{
+    public Reel[] reels; 
+
+    void Start()
+    {
+        
+    }
+
+    private bool AllStopped()
+    {
+        for (int i = 0; i < reels.Length; i++)
+        {
+            if (reels[i].isSpinning)
+            {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
