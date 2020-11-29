02e19411ce13257c6c29e4c0cae49833a0b6fd04
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-29 01:56:11 +0000

    Start all and declare enumerator

--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SlotMachine : MonoBehaviour
 {
-    public Reel[] reels; 
+    public Reel[] reels;
+    private IEnumerator enumerator;
 
     void Start()
     {
-        
+        enumerator = reels.GetEnumerator();
     }
 
     private bool AllStopped()
@@ -23,8 +24,29 @@ public class SlotMachine : MonoBehaviour
         return true;
     }
 
+    private void StartAll()
+    {
+        for (int i = 0; i < reels.Length; i++)
+        {
+            reels[i].isSpinning = true; 
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Rator = " + enumerator);
+
+            if (AllStopped())
+            {
+                StartAll();
+                enumerator.Reset();
+            }
+            else
+            {
+
+            }
+        }
     }
 }
