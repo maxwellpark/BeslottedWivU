0d412ac4346230764d3022ab89e282b67e9775a7
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-12-05 08:24:31 +0000

    Layer class skeleton

---
+++ Assets/Scripts/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer : MonoBehaviour
+{
+    public Reel[] reels;
+    public Layer layerBelow;
+
+    public bool isActive;
+
+    public Reel[] GetMatchesUnderneath()
+    {
+
+
+        return new Reel[] { };
+    }
+}
